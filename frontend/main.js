3854c20cd30f819439411d6db5276c640b06ce5b
Memo <gdelgado1@ucol.mx>
2026-02-17 14:53:13 -0600

    Nodes visualizer implemented

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,5 @@
+//import cytoscape from "cytoscape";
+
 var cy = cytoscape({
   container: document.getElementById("cy"),
 
@@ -35,48 +37,40 @@ var cy = cytoscape({
   },
 });
 
-cy.on("tap", "node", function (evt) {
-  const node = evt.target;
+let labels = ["a", "b", "c", "d", "e", "f", "g"];
+let added = ["a", "b", "c"];
 
-  alert(`Click en ${node.data("label")}\nInfo: ${node.data("info")}`);
-});
-
-labels = ["a", "b", "c", "d", "e", "f", "g"];
-added = ["a", "b", "c"];
-
-document.getElementById("hola").addEventListener("click", () => {
+document.getElementById("add-node-btn").addEventListener("click", () => {
   //while (true) {
-  let nuevo;
-  let nivel = 0;
-  let anterior = added[added.length - 1];
+  let newNode;
+  let level = 0;
+  let lastNode = added[added.length - 1];
 
   while (true) {
-    let otro = labels.findIndex((element) => element === anterior[nivel]);
-    //console.log(otro, "prueba");
-    if (otro === labels.length - 1 && nivel + 1 >= anterior.length) {
-      nivel += 1;
-      //Para añadir un nuevo nivel
-      if (anterior.length <= nivel + 1) {
-        aAgrgar = "";
-        for (let i = 0; i <= nivel; i++) {
-          aAgrgar += "a";
-        }
-        //console.log("QUEEEEE", aAgrgar);
-        added.push(aAgrgar);
-        break;
+    // index of the letter of the last addedNode
+    let letterLast = labels.findIndex((element) => element === lastNode[level]);
+
+    // if new labels run out and there is no next lebel in the last node (str.length)
+    if (letterLast === labels.length - 1 && level + 1 >= lastNode.length) {
+      level += 1;
+      let toAdd = "";
+      for (let i = 0; i <= level; i++) {
+        toAdd += "a";
       }
-    } else if (otro === labels.length - 1) {
-      anterior = anterior.split("");
-      anterior[nivel] = "a";
-      anterior = anterior.join("");
-      nivel += 1;
-    } else {
-      nuevo = anterior.split("");
-      nuevo[nivel] = labels[otro + 1];
-      added.push(nuevo.join(""));
+      added.push(toAdd);
       break;
-      //console.log("COMO", labels[otro + 1]);
-      //console.log(nuevo, "sAHUR");
+
+      //if labels run out but there are more letters in the last node
+    } else if (letterLast === labels.length - 1) {
+      lastNode = lastNode.split("");
+      lastNode[level] = "a";
+      lastNode = lastNode.join("");
+      level += 1;
+    } else {
+      newNode = lastNode.split("");
+      newNode[level] = labels[letterLast + 1];
+      added.push(newNode.join(""));
+      break;
     }
   }
 
@@ -92,6 +86,42 @@ document.getElementById("hola").addEventListener("click", () => {
 
   console.log("agregado", added);
   //}
+});
 
-  //cy.add([{}]);
+let toConnect = [];
+
+cy.on("tap", "node", (e) => {
+  const node = e.target;
+  //Adds the index of the existing node to a global array
+  toConnect.push(added.indexOf(node.data("id")));
+
+  //If there are two elements in the array an attempt is made to add a edge
+  if (toConnect.length === 2) {
+    //Sorts the array
+    if (toConnect[0] > toConnect[1]) {
+      var copy = toConnect[0];
+      toConnect[0] = toConnect[1];
+      toConnect[1] = copy;
+    }
+
+    let isConnection = cy.edges(
+      `[source = "${added[toConnect[0]]}"][ target = "${added[toConnect[1]]}"]`,
+    );
+    // if the edge does not exist the it is added
+    if (isConnection.length <= 0) {
+      cy.add({
+        group: "edges",
+        data: { source: added[toConnect[0]], target: added[toConnect[1]] },
+      });
+      toConnect = [];
+    } else {
+      console.log("Ya existe esa conecxion");
+      toConnect = [];
+    }
+  }
+});
+
+cy.on("tap", "edge", (e) => {
+  const clickeado = e.target;
+  cy.remove(clickeado);
 });
